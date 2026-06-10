edc32e79b0f700bcfdd37d25d3f1b453bc014372
turn53 <vyachesla.ivanov.0404@mail.ru>
2026-06-10 16:55:43 +0300

    fix static files

--- eco_shop/settings.py
+++ eco_shop/settings.py
@@ -137,9 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Uploaded content)
